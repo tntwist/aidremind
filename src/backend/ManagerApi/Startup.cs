aecaf04f266a339941ce853e325bc2cea5612841
Nico Lachmuth <nico@nlachmuth.de>
2020-03-21 21:05:24 +0100

    added missing parameter

--- src/backend/ManagerApi/Startup.cs
+++ src/backend/ManagerApi/Startup.cs
@@ -40,7 +40,7 @@ namespace ManagerApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AidRemindDbContext dbContext)
         {
             dbContext.Database.Migrate();
 
