f29070e34c0bcbea0ca80465e10ae9a438dcacfb
Nico Lachmuth <nico@nlachmuth.de>
2020-03-21 16:03:38 +0100

    added user controller

---
+++ src/backend/ClerkApi/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using ClerkApi.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ClerkApi.Controllers
+{
+    public class UserController : AbstractBaseController
+    {
+        public UserController()
+        {
+        }
+
+        [HttpPost]
+        public async Task<int> Create([FromBody]UserCreationDto user) 
+        {
+            return 1;
+        }
+    }
+}
