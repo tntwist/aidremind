a6177591a8bb7f02f16b658f9adb66fc88bd8de1
Alexander Keck <Alexander Keck>
2020-03-22 17:24:36 +0100

    implement acceptance handling

--- src/backend/Notifier/src/main/java/org/aidmind/reminder/PushNotificationResource.java
+++ src/backend/Notifier/src/main/java/org/aidmind/reminder/PushNotificationResource.java
@@ -11,6 +11,8 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.inject.Inject;
+import javax.jms.*;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
@@ -31,6 +33,9 @@ public class PushNotificationResource {
     @RestClient
     @Inject
     TaskActivityService taskActivityService;
+    @Inject
+    ConnectionFactory connectionFactory;
+
 
     @OnOpen
     public void onOpen(Session session, @PathParam("userId") Long userId) {
@@ -47,4 +52,27 @@ public class PushNotificationResource {
             sessions.get(userId).getAsyncRemote().sendText(taskActivityJson);
         });
     }
+
+    @OnMessage
+    public void onMessage(Session session, @PathParam("userId") Long userId, String taskActivityJson) throws JMSException {
+        Connection connection = connectionFactory.createConnection();
+        javax.jms.Session jmsSession = connection.createSession(false, javax.jms.Session.AUTO_ACKNOWLEDGE);
+        Queue queue = jmsSession.createQueue("task-accepted");
+        MessageProducer producer = jmsSession.createProducer(queue);
+        producer.send(jmsSession.createTextMessage(taskActivityJson));
+    }
+
+    @Incoming("task-accepted")
+    public void acceptTask(TaskActivity taskActivity) throws JsonProcessingException {
+        TaskActivity storedTaskActivity = this.taskActivityService.getById(taskActivity.getTaskActivityId());
+        String taskActivityJson = "";
+        if (null == storedTaskActivity.getUserId()) {
+            TaskActivity updatedTaskActivity = this.taskActivityService.update(taskActivity.getTaskActivityId(), taskActivity);
+            taskActivityJson = this.objectMapper.writeValueAsString(updatedTaskActivity);
+        } else {
+            taskActivityJson = this.objectMapper.writeValueAsString(storedTaskActivity);
+        }
+        sessions.get(taskActivity.getUserId()).getAsyncRemote().sendText(taskActivityJson);
+    }
+
 }
