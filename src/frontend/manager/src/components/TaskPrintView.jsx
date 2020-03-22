3b62a085b6a6a14b8be6a0a17a296fe15b1f4fc1
Johannes Schröder <jschroeder@uandi.com>
2020-03-22 22:56:54 +0100

    Add qrcode generation for tasks

---
+++ src/frontend/manager/src/components/TaskPrintView.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import './TaskPrintView.css'
+import * as QRCode from 'qrcodejs2';
+import { Button } from '@material-ui/core';
+
+export default function TaskPrintView({ task, category, onCloseDialog }) {
+
+    useEffect(() => {
+        function updateQrCode(text, stageId = 'qr') {
+            let qrEl = document.getElementById(stageId);
+            if (qrEl) {
+                // cleanup
+                while (qrEl.hasChildNodes()) {
+                    qrEl.removeChild(qrEl.lastChild);
+                }
+            }
+        
+            new QRCode(qrEl, {
+                text,
+                width: 128 * 2,
+                height: 128 * 2,
+                colorDark : '#000',
+                colorLight : '#fff',
+                correctLevel : QRCode.CorrectLevel.L
+            });
+        }
+        
+        updateQrCode('https://github.com/tntwist/aidremind');
+    })
+
+    return (
+        <div className="task-print-view">
+            <div className="task-print-view__stage">
+                <div className="task-print-view__category">{category.name}</div>
+                <div className="task-print-view__stage-body">
+                        <div className="task-print-view__title">{task.title}</div>
+                        <div id="qr" className="task-print-view__qrcode"></div>
+                        <Button className="task-print-view__close-dialog-button" variant="outlined" onClick={onCloseDialog}>Dialog schließen</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/frontend/manager/src/pages/CategoryView.jsx
+++ src/frontend/manager/src/pages/CategoryView.jsx
@@ -1,246 +1,231 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
-import CategoryApi from "../services/CategoryApi";
-import Alert from "@material-ui/lab/Alert";
-import EditIcon from "@material-ui/icons/Edit";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import TaskForm from "../components/TaskForm";
-import Paper from "@material-ui/core/Paper";
-import "./CategoryView.css";
-import { withStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import TaskApi from "../services/TaskApi";
-import format from "date-fns/format";
-import { getFrequencyLabel } from "../services/utils";
-import DeleteIcon from "@material-ui/icons/Delete";
-import CategoryForm from "../components/CategoryForm";
+import CategoryApi from '../services/CategoryApi';
+import Alert from '@material-ui/lab/Alert';
+import EditIcon from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import TaskForm from '../components/TaskForm';
+import Paper from '@material-ui/core/Paper';
+import './CategoryView.css'
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TaskApi from '../services/TaskApi';
+import format from 'date-fns/format';
+import { getFrequencyLabel } from '../services/utils';
+import DeleteIcon from '@material-ui/icons/Delete';
+import PrintIcon from '@material-ui/icons/Print';
+import CategoryForm from '../components/CategoryForm';
+import TaskPrintView from '../components/TaskPrintView';
+
 
 const StyledTableCell = withStyles(theme => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
-  },
-  body: {
-    fontSize: 16
-  }
-}))(TableCell);
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 16,
+    },
+  }))(TableCell);
 
-const StyledTableRow = withStyles(theme => ({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.background.default
-    }
-  }
-}))(TableRow);
+  const StyledTableRow = withStyles(theme => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.background.default,
+      },
+    },
+  }))(TableRow);
 
 function formatDate(date) {
-  if (!date) return null;
-  return format(new Date(date), "dd.MM.yy HH:mm");
+    if (!date) return null;
+    return format(new Date(date), 'dd.MM.yy HH:mm')
 }
 
 export default function CategoryView({ onTriggerRefresh }) {
-  const { categoryId } = useParams();
-  const [error, setError] = useState(null);
-  const [category, setCategory] = useState(null);
-  const histroy = useHistory();
-  const [activeForm, setActiveForm] = useState(null);
+    const { categoryId } = useParams();
+    const [error, setError] = useState(null);
+    const [category, setCategory] = useState(null);
+    const histroy = useHistory();
+    const [activeForm, setActiveForm] = useState(null);
+    const [printTask, setPrintTask] = useState(null);
 
-  const [tasks, setTasks] = useState(null);
-  const [refreshTrigger, setRefreshTrigger] = useState(new Date());
+    const [tasks, setTasks] = useState(null);
+    const [refreshTrigger, setRefreshTrigger] = useState(new Date());
 
-  function onTaskCreated() {
-    setActiveForm(null);
-    setRefreshTrigger(new Date());
-  }
+    function onTaskCreated() {
+        setActiveForm(null);
+        setRefreshTrigger(new Date());
+    }
+    
+    function onCategorySaved() {
+        onTriggerRefresh();
 
-  function onCategorySaved() {
-    onTriggerRefresh();
-
-    setActiveForm(null);
-    setRefreshTrigger(new Date());
-  }
-
-  useEffect(() => {
-    async function refreshCategory(id) {
-      try {
-        const category = await CategoryApi.getOneById(id);
-        setCategory(category);
-      } catch (err) {
-        setError(err);
-      }
+        setActiveForm(null);
+        setRefreshTrigger(new Date());
     }
 
-    async function refreshTasks(categoryId) {
-      console.log("Refresh tasks");
-      try {
-        const result = await TaskApi.getAllByCategoryId(categoryId);
-        setTasks(result);
-      } catch (err) {
-        setError(err);
-      }
+    useEffect(() => {
+        async function refreshCategory(id) {
+            try {
+                const category = await CategoryApi.getOneById(id);
+                setCategory(category);
+            } catch (err) {
+                setError(err);
+            }
+        }
+
+        async function refreshTasks(categoryId) {
+            console.log('Refresh tasks');
+            try {
+                const result = await TaskApi.getAllByCategoryId(categoryId);
+                setTasks(result);
+            } catch (err) {
+                setError(err);
+            }
+        }
+
+        refreshCategory(categoryId);
+        refreshTasks(categoryId);
+    }, [categoryId, refreshTrigger]);
+
+    async function deleteTask(t) {
+        try {
+            await TaskApi.delete(t.taskId);
+            setRefreshTrigger(new Date());
+        } catch (err) {
+            setError(err);
+        }
     }
-
-    refreshCategory(categoryId);
-    refreshTasks(categoryId);
-  }, [categoryId, refreshTrigger]);
-
-  async function deleteTask(t) {
-    try {
-      await TaskApi.delete(t.taskId);
-      setRefreshTrigger(new Date());
-    } catch (err) {
-      setError(err);
+    
+    async function deleteCategory() {
+        try {
+            await CategoryApi.delete(categoryId);
+            histroy.push(category.parentCategoryId ? `/category/${category.parentCategoryId}` : '/');
+            onTriggerRefresh();
+        } catch (err) {
+            setError(err);
+        }
     }
-  }
+    
+    return (
+        <div className="category-view">
+            { error && (
+                <Alert severity="error" variant="filled">{error.message}</Alert>
+            )}
+            {!category && !error && (
+                <div>Loading ...</div>
+            )}
+            {category && (
+                <>
+                    { activeForm !== 'edit' && (
+                        <Paper className="category-view__head">
+                            <div>
+                                <h1 className="category-view__name">{ category.name }</h1>
+                                <div className="category-view__description">{ category.description }</div>
+                            </div>
+                            <div className="category-view__actions">
+                                <Button
+                                    size="small"
+                                    variant="outlined"
+                                    startIcon={<EditIcon/>}
+                                    onClick={() => setActiveForm('edit')}
+                                    >
+                                    Bearbeiten
+                                </Button>
+                                <Button
+                                    size="small"
+                                    variant="outlined"
+                                    startIcon={<DeleteIcon/>}
+                                    color="secondary"
+                                    onClick={deleteCategory}
+                                    >
+                                    Entfernen
+                                </Button>
+                            </div>
+                        </Paper>
+                    )}
+                    { activeForm === 'edit' && (
+                        <CategoryForm
+                            category={category}
+                            onCategorySaved={onCategorySaved}
+                            onCloseForm={() => setActiveForm(null)}
+                        />
+                    )}
 
-  async function deleteCategory() {
-    try {
-      await CategoryApi.delete(categoryId);
-      histroy.push(
-        category.parentCategoryId
-          ? `/category/${category.parentCategoryId}`
-          : "/"
-      );
-      onTriggerRefresh();
-    } catch (err) {
-      setError(err);
-    }
-  }
+                    { !activeForm && (
+                        <div className="category-view__main-actions">
+                            <Button variant="contained" color="primary"  onClick={() => setActiveForm('task')}>Aufgabe hinzufügen</Button>
+                            <Button variant="outlined" color="primary"  onClick={() => setActiveForm('subcategory')}>Unterkategorie hinzufügen</Button>
+                        </div>
+                    )}
 
-  return (
-    <div className="category-view">
-      {error && (
-        <Alert severity="error" variant="filled">
-          {error.message}
-        </Alert>
-      )}
-      {!category && !error && <div>Loading ...</div>}
-      {category && (
-        <>
-          {activeForm !== "edit" && (
-            <Paper className="category-view__head">
-              <div>
-                <h1 className="category-view__name">{category.name}</h1>
-                <div className="category-view__description">
-                  {category.description}
-                </div>
-              </div>
-              <div className="category-view__actions">
-                <Button
-                  size="small"
-                  variant="outlined"
-                  startIcon={<EditIcon />}
-                  onClick={() => setActiveForm("edit")}
-                >
-                  Bearbeiten
-                </Button>
-                <Button
-                  size="small"
-                  variant="outlined"
-                  startIcon={<DeleteIcon />}
-                  color="secondary"
-                  onClick={deleteCategory}
-                >
-                  Entfernen
-                </Button>
-              </div>
-            </Paper>
-          )}
-          {activeForm === "edit" && (
-            <CategoryForm
-              category={category}
-              onCategorySaved={onCategorySaved}
-              onCloseForm={() => setActiveForm(null)}
-            />
-          )}
+                    { activeForm === 'task' && (
+                        <TaskForm
+                            categoryId={categoryId}
+                            onTaskCreated={onTaskCreated}
+                            onCloseForm={() => setActiveForm(null)}
+                        />
+                    )}
 
-          {!activeForm && (
-            <div className="category-view__main-actions">
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => setActiveForm("task")}
-              >
-                Aufgabe hinzufügen
-              </Button>
-              <Button
-                variant="outlined"
-                color="primary"
-                onClick={() => setActiveForm("subcategory")}
-              >
-                Unterkategorie hinzufügen
-              </Button>
-            </div>
-          )}
+                    { activeForm === 'subcategory' && (
+                        <CategoryForm
+                            parentCategoryId={categoryId}
+                            onCategorySaved={onCategorySaved}
+                            onCloseForm={() => setActiveForm(null)}
+                        />
+                    )}
+                </>
+            )}
 
-          {activeForm === "task" && (
-            <TaskForm
-              categoryId={categoryId}
-              onTaskCreated={onTaskCreated}
-              onCloseForm={() => setActiveForm(null)}
-            />
-          )}
+            {tasks && tasks.length > 0 && <TasksTable tasks={tasks} deleteTask={deleteTask} printTask={setPrintTask} />}
 
-          {activeForm === "subcategory" && (
-            <CategoryForm
-              parentCategoryId={categoryId}
-              onCategorySaved={onCategorySaved}
-              onCloseForm={() => setActiveForm(null)}
-            />
-          )}
-        </>
-      )}
-
-      {tasks && tasks.length > 0 && (
-        <TasksTable tasks={tasks} deleteTask={deleteTask} />
-      )}
-    </div>
-  );
+            {printTask && <TaskPrintView task={printTask} category={category} onCloseDialog={() => setPrintTask(null)} />}
+        </div>
+    )
 }
 
-function TasksTable({ tasks, deleteTask }) {
-  return (
-    <TableContainer component={Paper} className="category-view__table">
-      <Table>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Aufgabe</StyledTableCell>
-            <StyledTableCell>Start</StyledTableCell>
-            <StyledTableCell>End</StyledTableCell>
-            <StyledTableCell>Frequency</StyledTableCell>
-            <StyledTableCell>Aktion</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tasks.map(task => (
-            <StyledTableRow key={task.id}>
-              <StyledTableCell component="th" scope="row">
-                <div className="category-view__task-title">{task.title}</div>
-                <div className="category-view__task-description">
-                  {task.description}
-                </div>
-              </StyledTableCell>
-              <StyledTableCell>{formatDate(task.startDate)}</StyledTableCell>
-              <StyledTableCell>{formatDate(task.endDate)}</StyledTableCell>
-              <StyledTableCell>
-                {getFrequencyLabel(task.frequency)}
-              </StyledTableCell>
-              <StyledTableCell>
-                <IconButton size="small" onClick={() => deleteTask(task)}>
-                  <DeleteIcon />
-                </IconButton>
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+function TasksTable({ tasks, deleteTask, printTask }) {
+    return (
+        <TableContainer component={Paper} className="category-view__table">
+            <Table>
+            <TableHead>
+                <TableRow>
+                    <StyledTableCell>Aufgabe</StyledTableCell>
+                    <StyledTableCell>Start</StyledTableCell>
+                    <StyledTableCell>End</StyledTableCell>
+                    <StyledTableCell>Frequency</StyledTableCell>
+                    <StyledTableCell>Aktion</StyledTableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {tasks.map(task => (
+                    <StyledTableRow key={task.id}>
+                        <StyledTableCell component="th" scope="row">
+                            <div className="category-view__task-title">{task.title}</div>
+                            <div className="category-view__task-description">{task.description}</div>
+                        </StyledTableCell>
+                        <StyledTableCell>{formatDate(task.startDate)}</StyledTableCell>
+                        <StyledTableCell>{formatDate(task.endDate)}</StyledTableCell>
+                        <StyledTableCell>{getFrequencyLabel(task.frequency)}</StyledTableCell>
+                        <StyledTableCell>
+                            <IconButton size="small" onClick={() => printTask(task)}>
+                                <PrintIcon />
+                            </IconButton>
+                            <IconButton size="small" onClick={() => window.confirm('Aufgabe entfernen?') ? deleteTask(task) : null}>
+                                <DeleteIcon />
+                            </IconButton>
+                        </StyledTableCell>
+                    </StyledTableRow>
+                ))}
+            </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
