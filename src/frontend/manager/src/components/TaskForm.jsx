f7e4b7ce86239f7f2d32a55de09e181320ac9cf7
Johannes Schröder <jschroeder@uandi.com>
2020-03-22 19:24:04 +0100

    Add task form

--- src/frontend/manager/src/components/TaskForm.jsx
+++ src/frontend/manager/src/components/TaskForm.jsx
@@ -1,50 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { useForm } from "react-hook-form";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import UpdateIcon from '@material-ui/icons/Update';
 import './TaskForm.css';
+import DateFnsUtils from '@date-io/date-fns'; // choose your lib
+import {
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TaskApi from '../services/TaskApi';
 
-// import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-// import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-// import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+const frequencyOptions = [
+    { value: '*/10 * * * *', label: 'Alle 10min' },
+    { value: '*/15 * * * *', label: 'Alle 15min' },
+    { value: '*/30 * * * *', label: 'Alle 30min ' },
+    { value: '* */1 * * *', label: 'Jede volle Stunde' },
+    { value: '* */2 * * *', label: 'Alle 2 Stunden' },
+    { value: '* */3 * * *', label: 'Alle 3 Stunden' },
+    { value: '* */4 * * *', label: 'Alle 4 Stunden' },
+    { value: '* */8 * * *', label: 'Alle 8 Stunden' },
+    { value: '* * */1 * *', label: 'Jeden Tag' },
+    { value: '* * */2 * *', label: 'Alle 2 Tage' },
+    { value: '* * */3 * *', label: 'Alle 3 Tage' },
+    { value: '* * */4 * *', label: 'Alle 4 Tage' },
+    { value: '* * */5 * *', label: 'Alle 5 Tage' },
+    { value: '* * */6 * *', label: 'Alle 6 Tage' },
+    { value: '* * */7 * *', label: 'Alle 7 Tage' },
+];
 
 export default function TaskForm({ categoryId, onTaskCreated }) {
     const { register, handleSubmit, errors, reset } = useForm();
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    const [frequency, setFrequency] = useState(null);
 
-    async function onSubmit() {
-        console.log('Save task');
+    async function onSubmit(data) {
+        try {
+            const newTask = {
+                categoryId,
+                title: data.title,
+                description: data.description,
+                startDate,
+                endDate,
+                frequency: frequency ? frequency.value : null,
+            };
+
+            await TaskApi.save(newTask);
+            reset();
+            setStartDate(null);
+            setEndDate(null);
+            setFrequency(null);
+        } catch (err) {
+            console.error(err);
+        }
     }
-
-    // return (
-    //     <ExpansionPanel>
-    //         <ExpansionPanelSummary
-    //             expandIcon={<ExpandMoreIcon />}
-    //             id="panel-header"
-    //         >
-    //             Neue Aufgabe hinzufügen
-    //         </ExpansionPanelSummary>
-    //         <ExpansionPanelDetails>
-    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-    //             sit amet blandit leo lobortis eget.
-    //         </ExpansionPanelDetails>
-    //     </ExpansionPanel>
-    // );
-
-    // TODO: Continue
 
     return (
         <Paper className="task-form">
             <form onSubmit={handleSubmit(onSubmit)} className="task-form__body">
+                <div className="task-form__title">Neue Aufgabe anlegen</div>
                 <TextField
-                    id="categoryName"
-                    name="categoryName"
-                    label="Name"
+                    id="title"
+                    name="title"
+                    label="Aufgabe"
                     variant="outlined"
                     inputRef={register({ required: true })}
-                    error={errors.categoryName ? true : false}
-                    helperText={errors.categoryName && errors.categoryName.type === 'required' ? 'Dies ist ein Pflichtfeld' : ''}
+                    error={errors.title ? true : false}
+                    helperText={errors.title && errors.title.type === 'required' ? 'Die Aufgabe benötigt einen Titel' : ''}
                     required
                 />
                 <TextField
@@ -54,12 +79,64 @@ export default function TaskForm({ categoryId, onTaskCreated }) {
                     variant="outlined"
                     inputRef={register()}
                     multiline
-                    rows="4"
+                    rows="6"
                 />
+
+                <div className="task-form__dates">
+                    <UpdateIcon/>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        
+                        <DateTimePicker
+                            id="startDate"
+                            name="startDate"
+                            label="Start Datum"
+                            ampm={false}
+                            autoOk={true}
+                            minutesStep={5}
+                            value={startDate}
+                            disableToolbar={true}
+                            showTodayButton
+                            disablePast
+                            clearable
+                            inputVariant="outlined"
+                            format={'dd.MM.yy HH:mm'}
+                            onChange={setStartDate}
+                        />
+
+                        <DateTimePicker
+                            id="endDate"
+                            name="endDate"
+                            label="End Datum"
+                            ampm={false}
+                            autoOk={true}
+                            minutesStep={5}
+                            value={endDate}
+                            disableToolbar={true}
+                            showTodayButton
+                            disablePast
+                            clearable
+                            minDate={startDate}
+                            inputVariant="outlined"
+                            format={'dd.MM.yy HH:mm'}
+                            onChange={setEndDate}
+                        />
+                    </MuiPickersUtilsProvider>
+
+                    <Autocomplete
+                        id="frequency"
+                        name="frequency"
+                        label="Wiederhungen"
+                        options={frequencyOptions}
+                        getOptionLabel={option => option.label}
+                        renderInput={params => <TextField {...params} label="Wiederhungen" variant="outlined" />}
+                        value={frequency}
+                        onChange={(event, value) => setFrequency(value)}
+                    />
+                </div>
 
                 <div className="category-form__footer">
                     <Button variant="contained" color="primary" type="submit">
-                        {categoryId ? 'Aktualisieren' : 'Hinzufügen'}
+                        Hinzufügen
                     </Button>
                 </div>
             </form>
