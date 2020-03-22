46db47e77d01ef014a6dc2913150bea5de3b33fe
Nico Lachmuth <nico@nlachmuth.de>
2020-03-22 22:18:13 +0100

    use put to update entities

--- src/frontend/manager/src/services/AbstractResource.js
+++ src/frontend/manager/src/services/AbstractResource.js
@@ -1,42 +1,51 @@
-import Axios from 'axios';
+import Axios from "axios";
 export default class AbstractResource {
-    constructor(resourcePath, idKey) {
-        this.resourcePath = resourcePath;
-        this.idKey = idKey;
-    }
-    /**
-     * Get one resource object
-     * @param {number} id 
-     */
-    async getOneById(id) {
-        const result = await Axios.get(`${this.resourcePath}/${id}`);
-        return result.data;
-    }
+  constructor(resourcePath, idKey) {
+    this.resourcePath = resourcePath;
+    this.idKey = idKey;
+  }
+  /**
+   * Get one resource object
+   * @param {number} id
+   */
+  async getOneById(id) {
+    const result = await Axios.get(`${this.resourcePath}/${id}`);
+    return result.data;
+  }
 
-    /**
-     * Get all resource objects
-     */
-    async getAll() {
-        const result = await Axios.get(this.resourcePath);
-        return result.data;
-    }
+  /**
+   * Get all resource objects
+   */
+  async getAll() {
+    const result = await Axios.get(this.resourcePath);
+    return result.data;
+  }
 
-    /**
-     * Save one resource object
-     * @param {*} resourceObject 
-     */
-    async save(resourceObject) {
-        const id = resourceObject[this.idKey];
-        const suffix = id ? `/${id}` : '';
-        const result = await Axios.post(`${this.resourcePath}${suffix}`, resourceObject);
-        return result.data;
-    }
+  /**
+   * Save one resource object
+   * @param {*} resourceObject
+   */
+  async save(resourceObject) {
+    const id = resourceObject[this.idKey];
+    const suffix = id ? `/${id}` : "";
 
-    /**
-     * Delete one resource object
-     * @param {number} id 
-     */
-    delete(id) {
-        return Axios.delete(`${this.resourcePath}/${id}`);
+    let result;
+    if (id) {
+      result = await Axios.put(`${this.resourcePath}${suffix}`, resourceObject);
+    } else {
+      result = await Axios.post(
+        `${this.resourcePath}${suffix}`,
+        resourceObject
+      );
     }
+    return result.data;
+  }
+
+  /**
+   * Delete one resource object
+   * @param {number} id
+   */
+  delete(id) {
+    return Axios.delete(`${this.resourcePath}/${id}`);
+  }
 }
