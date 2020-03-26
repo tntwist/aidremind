cccd8580c5cf7bbcfeb5ca27e517acac73b932ef
Alexander Keck <Alexander Keck>
2020-03-26 16:50:36 +0100

    ignore null in json

--- src/backend/Notifier/src/main/java/org/aidmind/reminder/model/TaskActivity.java
+++ src/backend/Notifier/src/main/java/org/aidmind/reminder/model/TaskActivity.java
@@ -1,19 +1,24 @@
 package org.aidmind.reminder.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Date;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TaskActivity {
 
     private final Integer taskActivityId;
     private final Integer taskId;
     private final Integer userId;
     private final Date dueToDate;
+    private final Date doneDate;
 
-    public TaskActivity(final Integer taskActivityId, final Integer taskId, final Integer userId, final Date dueToDate) {
+    public TaskActivity(final Integer taskActivityId, final Integer taskId, final Integer userId, final Date dueToDate, final Date doneDate) {
         this.taskActivityId = taskActivityId;
         this.taskId = taskId;
         this.userId = userId;
         this.dueToDate = dueToDate;
+        this.doneDate = doneDate;
     }
 
     public Integer getTaskActivityId() {
@@ -30,5 +35,9 @@ public class TaskActivity {
 
     public Date getDueToDate() {
         return this.dueToDate;
+    }
+
+    public Date getDoneDate() {
+        return this.doneDate;
     }
 }
